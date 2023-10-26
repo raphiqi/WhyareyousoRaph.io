392c5275afee189bbd369cced4fcb76981d2d107
Raphael Warui <124355731+raphiqi@users.noreply.github.com>
2023-10-26 11:39:47 +0300

    php file

--- email prompt.php
+++ email prompt.php
@@ -3,9 +3,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
-    $to = "raphael.warui.k@gmail.com";
-    $subject = "New Contact Form Submission from $name";
-    $headers = "From: $email";
-    mail($to, $subject, $message, $headers);
+    
+    if (empty($name) || empty($email) || empty($message)) {
+        // Handle empty fields - you can display an error message
+        echo "Please fill in all fields.";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // Handle invalid email format - you can display an error message
+        echo "Invalid email format.";
+    } else {
+        $to = "raphael.warui.k@gmail.com";
+        $subject = "New Contact Form Submission from $name";
+        $headers = "From: $email";
+
+        if (mail($to, $subject, $message, $headers)) {
+            // Email sent successfully - display a confirmation message
+            echo "Thank you for your message. We will get back to you soon.";
+        } else {
+            // Error sending email - display an error message
+            echo "Sorry, there was an issue sending your message. Please try again later.";
+        }
+    }
 }
+
 
